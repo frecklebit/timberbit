f5eed9887c625540940c276771bfde20e85db4b2
Adam Jenkins <jenkinsac@health.missouri.edu>
2017-04-24 09:00:47 -0500

    Cleanup

--- index.php
+++ index.php
@@ -16,8 +16,11 @@
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $context['foo'] = 'bar';
+
 $templates = array( 'index.twig' );
+
 if ( is_home() ) {
 	array_unshift( $templates, 'home.twig' );
 }
+
 Timber::render( $templates, $context );
